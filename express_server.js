863f6ce6d5ce89d41106c9f2775ed0cb84bfd7e9
jimyaragon <jimy.aragon@gmail.com>
2023-08-12 21:23:31 -0600

    Redirecting URL correctly

--- express_server.js
+++ express_server.js
@@ -4,6 +4,8 @@ const PORT = 8080; // default port 8080;
 
 app.set("view engine", "ejs");
 
+app.use(express.urlencoded({ extended: true }));
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -30,7 +32,37 @@ app.get("/urls.json", (req, res) => {
     res.render("urls_index", templateVars);
   });
 
+  app.get("/urls/new", (req, res) => {
+    res.render("urls_new");
+  });
+  
   app.get("/urls/:id", (req, res) => {
     const templateVars = { id: req.params.id, longURL: "http://www.lighthouselabs.ca"};
     res.render("urls_show", templateVars);
+  });
+
+  app.post("/urls", (req, res) => {
+    const randomId = generateRandomString();
+    urlDatabase[randomId] = req.body.longURL; // Save the new id-longURL pair
+    res.redirect(`/urls/${randomId}`); // Redirect to the new URL
+  });
+
+  function generateRandomString() {
+    const length = 6;
+    const characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+    let randomString = '';
+    for (let i = 0; i < length; i++) {
+      const randomIndex = Math.floor(Math.random() * characters.length);
+      randomString += characters[randomIndex];
+    }
+    return randomString;
+  }
+
+  app.get("/u/:id", (req, res) => {
+    const longURL = urlDatabase[req.params.id];
+    if (longURL) {
+      res.redirect(longURL);
+    } else {
+      res.status(404).send("Short URL not found");
+    }
   });
